7445c0433f875b96ba61f1bc5a70a71e6c3322e7
openmetaloom <openmetaloom@gmail.com>
2026-02-03 17:09:41 +0000

    fix: Use base path config for vite build

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
     port: 5173,
     proxy: {
@@ -19,12 +17,11 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
   },
-  publicDir: 'public',
 })
