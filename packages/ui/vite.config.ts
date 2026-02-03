3fb50f9377cb6b8611977f2773b0df0f8e6e89b9
openmetaloom <openmetaloom@gmail.com>
2026-02-03 17:16:29 +0000

    Add rollup input config for vite build

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '',
   server: {
     port: 5173,
     proxy: {
@@ -23,5 +23,10 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+      },
+    },
   },
 })
